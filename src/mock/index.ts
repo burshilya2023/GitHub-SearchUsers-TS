98c69269356fb8c1bd218ca8c6cecdb8970e6591
bursh2021 <115107442+burshilya2023@users.noreply.github.com>
2022-10-26 11:30:07 +0300

    Initial commit

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  "login": "burshIlya2023",
+  "avatar": "https://avatars.githubusercontent.com/u/115107442?s=400&u=abfc30e61921dd599e5bb2ec71ed67820aba5808&v=4",
+  "name": "Ilya Bursh",
+  "company": "none",
+  "blog": "bursh",
+  "location": "Minsk",
+  "bio": "React",
+  "twitter": "none",
+  "repos": 1,
+  "followers": 0,
+  "following": 0,
+  "created": "September 2022",
+}
